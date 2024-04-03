d6f877474d7c07f9534801db2bbae805f71aab10
CookiePawn <cookiepawn@naver.com>
2024-04-03 16:13:32 +0900

    TRU-59
    1. 클라이언트 - 서버 통신 코드 작성

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-const axios = require('axios');
+const bodyParser = require('body-parser');
 
 //크롤링
 const scrapingFunction = require('./utils/scrapingAssets');
@@ -11,6 +11,8 @@ const scrapingFunction = require('./utils/scrapingAssets');
 
 const app = express();
 const server = http.createServer(app);
+// JSON 파싱을 위한 미들웨어 추가
+app.use(bodyParser.json());
 
 dotenv.config(); // dotenv를 사용하여 환경 변수 로드 (env)
 
@@ -27,19 +29,11 @@ connection.connect();
 
 
 //번개장터 크롤링 2시간 간격으로 주기적으로 실행
-setInterval(() => {
-    scrapingFunction.scrapingBJ(connection, axios, process.env.OPENAI_KEY);
-}, 10 * 60 * 1000); // 2시간
+// setInterval(() => {
+//     scrapingFunction.scrapingBJ(connection, axios, process.env.OPENAI_KEY);
+// }, 10 * 60 * 1000); // 2시간
 
-
-scrapingFunction.scrapingBJ(connection, axios, process.env.OPENAI_KEY);
-//conditionTraining.conditionTraining();
-
-
-
-
-//특수문자 제거 
-//.replace(/[^\w\s가-힣ㄱ-ㅎㅏ-ㅣ]/g, '')
+// scrapingFunction.scrapingBJ(connection, axios, process.env.OPENAI_KEY);
 
 
 
@@ -57,6 +51,59 @@ app.get('/getInfo', (req, res) => {
     connection.query('SELECT * FROM AssetsMoreInfo', (error, results, fields) => {
         if (error) throw error;
         res.json(results);
+    });
+});
+
+// 로그인 확인
+app.post('/fetchLogin', (req, res) => {
+    const { nickname, email, profileImg } = req.body;
+
+    // 가입 확인
+    connection.query(`SELECT * FROM User WHERE EMAIL=${mysql.escape(email)}`, (error, results, fields) => {
+        if (error) {
+            throw error;
+        }
+        
+        if (results.length > 0) {
+            // 계정이 존재하는 경우
+            res.json({ nickname, email, profileImg, userID: results[0].UserID });
+        } else {
+            // 계정이 존재하지 않는 경우 회원가입 진행
+            const insertQuery = 'INSERT INTO User (NICKNAME, EMAIL, ProfileImage, DATE) VALUES (?, ?, ?, ?)';
+            const insertValues = [nickname, email, profileImg, new Date()];
+
+            connection.query(insertQuery, insertValues, (error, results, fields) => {
+                if (error) {
+                    throw error;
+                }
+                // 회원가입 완료 응답
+                res.json('회원가입 완료');
+            });
+        }
+    });
+});
+
+//자산 로드
+app.post('/fetchUserAssets', (req, res) => {
+    const { nickname, email, profileImg } = req.body;
+
+    //아이디값 확인
+    connection.query(`SELECT * FROM User WHERE EMAIL=${mysql.escape(email)}`, (error, results, fields) => {
+        if (error) {
+            throw error;
+        }
+
+        if (results.length > 0) {
+            // 계정이 존재하는 경우
+            connection.query(`SELECT * FROM Assets WHERE UserID=${results[0].UserID}`, (error, results, fields) => {
+                if (error) throw error;
+                res.json(results);
+            });
+
+        } else {
+            // 계정이 존재하지 않는 경우
+            res.json('데이터 로드 실패! 로그인을 해주세요')
+        }
     });
 });
 
